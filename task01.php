e76bee1f0c785432abfd81bcae547043924d2d95
OussamaDouadi <104687081+oussamadouadi1@users.noreply.github.com>
2023-06-17 17:13:19 +0100

    php-assignment-01

---
+++ task01.php
@@ -0,0 +1,84 @@
+
+<!--Assignment Number 01 -->
+
+<?php 
+
+// This is a single line  comment
+/*
+
+This is a multiole line comment 
+here you can 
+write many things on 
+different lines
+
+*/
+?>
+<!-- This is a comment -->
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="<?php echo "UTF-8"; ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?="My First PHP Page" ?></title>
+</head>
+<body>
+    <div> <?php echo "We Love"; ?></div>
+    <div><?php echo "Elzero Channel"; ?></div>
+  </body>
+</html>
+  -->
+
+<!-- Assignment Number 02 -->
+<?php 
+
+// echo 'Prevent Me From Running Please';
+# echo 'Prevent Me From Running Please';
+/* echo 'Prevent Me From Running Please'; */
+
+?>
+
+<!-- Assignment Number 03 -->
+<?php
+
+
+/*
+My Application
+Version 1.0
+Created By Elzero 
+*/
+
+?>
+<!-- Assignment Number 04 -->
+<?php
+
+// ## First Comment
+/* 
+This comment is valid because it is 
+a single line comment that starts with "//" and
+anything after "//" on the same line is considered 
+a comment and cannot be executed as a code */
+
+# // # Second Comment
+
+/* 
+This comment is valid, it is a single line comment
+ starts with # and anything after # is considered as a comment 
+ and cannot be executed as a code */
+
+/* /* /* Third Comment */
+/*
+Valid and the /* after the first /* are not considered as nested comment unless we close the comment.
+*/
+
+/////// Fourth Comment
+
+/*
+Valid,  each slash / after // is considered a comment
+*/
+
+
+?>
+
+
